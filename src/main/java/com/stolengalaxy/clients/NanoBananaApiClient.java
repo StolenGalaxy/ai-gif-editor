77210a331473b05f96932fb52844ca517e1e47d5
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-03-02 21:18:06 +0000

    Add generation polling

--- src/main/java/com/stolengalaxy/clients/NanoBananaApiClient.java
+++ src/main/java/com/stolengalaxy/clients/NanoBananaApiClient.java
@@ -13,7 +13,7 @@ public class NanoBananaApiClient {
         return Requests.addHeader(request, "Authorization", AppConfig.NanoBananaAPIKey);
     }
 
-    public static Request imageToImageRequest(String imageURL, String promptText){
+    private static Request imageToImageRequest(String imageURL, String promptText){
         // "IMAGETOIAMGE" [sic]
         String json = String.format("""
                 {
@@ -31,19 +31,42 @@ public class NanoBananaApiClient {
         return authenticateRequest(requestBody);
     }
 
-    public static String checkGeneration(String taskID){
+    public static String imageToImage(String imageURL, String promptText){
+        Request request = imageToImageRequest(imageURL, promptText);
+        JsonObject response = Requests.sendRequest(request);
+
+        return response.get("data").getAsJsonObject().get("taskId").getAsString();
+    }
+
+    private static String checkGeneration(String taskID){
         Request request = Requests.generateRequestFromJson(endpoint + String.format("record-info?taskId=%s", taskID), "", false);
         request = authenticateRequest(request);
         JsonObject response = Requests.sendRequest(request);
 
-        System.out.println(response);
         int successFlag = response.get("data").getAsJsonObject().get("successFlag").getAsInt();
 
         return switch(successFlag){
             case 1 -> response.get("data").getAsJsonObject().get("response").getAsJsonObject().get("resultImageUrl").getAsString();
-            case 2 -> "TASK_CREATION_FAILED";
-            case 3 -> "GENERATION_FAILED";
+            case 2 -> "Error: Task creation failed";
+            case 3 -> "Error: Image generation failed";
             default -> "GENERATING";
         };
+    }
+
+    public static String checkUntilOutcome(String taskID, int delay){
+        String status = checkGeneration(taskID);
+        while(status.contains("GENERATING")){
+            status = checkGeneration(taskID);
+            try{
+                Thread.sleep(delay * 1000);
+            } catch (InterruptedException e){
+                throw new RuntimeException("There was an error while paused.", e);
+            }
+        }
+        if(status.contains("https://")){
+            return status;
+        } else{
+            throw new RuntimeException(status);
+        }
     }
 }
